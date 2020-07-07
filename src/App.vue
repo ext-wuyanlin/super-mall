55aefc034337c1f8c599f52ae855356b03960a9e
ext-wuyanlin <wyl19910205@hotmail.com>
2020-07-07 20:07:09 +0800

    '初始化项目'

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-07-07 18:51:49
+ * @LastEditTime: 2020-07-07 19:31:07
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \demo1\supermall\src\App.vue
+--> 
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-07-07 19:32:55
+ * @LastEditTime: 2020-07-07 19:54:42
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \demo1\supermall\src\router\index.js
+ */ 
+import Vue from 'vue'
+import Router from 'router'
+
